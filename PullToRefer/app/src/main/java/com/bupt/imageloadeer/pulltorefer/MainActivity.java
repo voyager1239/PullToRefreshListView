903a9f1f8d54160effc0787499f45fdd6d211f20
lianghenghui <liangheng1239@163.com>
2016-05-06 19:24:08 +0800

    下拉刷新实现

---
+++ PullToRefer/app/src/main/java/com/bupt/imageloadeer/pulltorefer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.bupt.imageloadeer.pulltorefer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RefreshableView refreshableView;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] items = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        refreshableView = (RefreshableView)findViewById(R.id.refreshable_view);
+        listView = (ListView) findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,items);
+        listView.setAdapter(adapter);
+        refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+            }
+        },0);
+    }
+}
